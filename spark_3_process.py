22d7d8470c4585ac44adc591574ac81f287fa714
Sergey Astakhov <sergey.astakhov@glowbyteconsulting.com>
2024-02-24 23:20:38 +0300

    spark done

--- spark_3_process.py
+++ spark_3_process.py
@@ -1,8 +1,9 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import *
-import json
+# from pyspark.sql.types import *
+# import json
+import time
 
-sparkSession = SparkSession.builder.appName("cw_process_2").getOrCreate()
+sparkSession = SparkSession.builder.appName("spark_sql_app_2").config("spark.eventLog.enabled", "true").getOrCreate()
 
 data = sparkSession.read.load("hdfs://namenode:9000/master.csv", format="csv",sep=",", inferSchema="true", header="true")
 data.registerTempTable("master")
@@ -17,15 +18,23 @@ result = sparkSession.sql("""select m.master_id, m.master_desc, o.order_id, o.or
 from master m JOIN order o ON o.order_master_id = m.master_id LEFT JOIN customer c ON o.order_customer_id = c.order_customer_id
 where o.order_fact_completion_date > o.order_due_date
 """)
+result.show()
 # result.explain()
 result.write.csv(path='hdfs://namenode:9000/result.csv',mode='overwrite', header=True)
 
 print("=== before stop ===")
 # input()
 print("=== after stop ===")
-sparkSession.stop()
+# sparkSession.stop()
+# time.sleep(10)
 print("=== after stop ===")
 
-# /spark/bin/pyspark
+# /spark/bin/pyspark  --master spark://spark-master:7077
 # data = sparkSession.read.load("hdfs://namenode:9000/result.csv", format="csv",sep=",", inferSchema="true", header="true")
 # data.show(20, False)
+
+
+# sparkSession.sql("""select count(1)
+# from master m JOIN order o ON o.order_master_id = m.master_id LEFT JOIN customer c ON o.order_customer_id = c.order_customer_id
+# where o.order_fact_completion_date <= o.order_due_date
+# """).show()
